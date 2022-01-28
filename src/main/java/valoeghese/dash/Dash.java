7e508cd9fa892f73d09af9fdea1738b7fc4f4a58
Valoeghese <valoeghesemods@gmail.com>
2022-01-29 01:29:27 +1300

    did it work? almost. what did it cost? my sleep.

--- src/main/java/valoeghese/dash/Dash.java
+++ src/main/java/valoeghese/dash/Dash.java
@@ -7,6 +7,8 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.core.Direction;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +27,7 @@ public class Dash implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("*dashing noises*");
 
-		ServerPlayNetworking.registerGlobalReceiver(DASH_PACKET, (client, player, handler, buf, responseSender) -> {
+		ServerPlayNetworking.registerGlobalReceiver(DASH_PACKET, (server, player, handler, buf, responseSender) -> {
 			long time = player.level.getGameTime();
 			DashTracker tracker = (DashTracker) player;
 
@@ -34,19 +36,21 @@ public class Dash implements ModInitializer {
 				player.resetAttackStrengthTicker();
 
 				Vec3 look = player.getLookAngle();
+				byte dir = buf.readByte();
+				//System.out.println(dir + " " + look + " " + Thread.currentThread());
 
-				switch (buf.readByte()) {
+				switch (dir) {
 				case FORWARD:
-					player.push(look.x, 0.1, look.z);
+					player.push(look.x, 0.3, look.z);
 					break;
 				case BACKWARDS:
-					player.push(-look.x, 0.1, -look.z);
+					player.push(-look.x, 0.3, -look.z);
 					break;
 				case LEFT:
-					player.push(-look.z, 0.1, look.x);
+					player.push(-look.z, 0.3, look.x);
 					break;
 				case RIGHT:
-					player.push(look.z, 0.1, -look.x);
+					player.push(look.z, 0.3, -look.x);
 					break;
 				}
 			}
