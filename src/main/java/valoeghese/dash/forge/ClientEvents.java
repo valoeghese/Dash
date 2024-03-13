10ff931426f0046a1e0a4a3f6446c24914fa475e
Valoeghese <valoeghesemods@gmail.com>
2024-03-13 22:37:04 +1300

    fix: fix server-side dash crash

--- src/main/java/valoeghese/dash/forge/ClientEvents.java
+++ src/main/java/valoeghese/dash/forge/ClientEvents.java
@@ -2,11 +2,17 @@ package valoeghese.dash.forge;
 
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ClientRegistry;
+import net.minecraftforge.client.ConfigGuiHandler;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.network.NetworkEvent;
 import valoeghese.dash.adapter.client.S2CContext;
+import valoeghese.dash.client.screen.DashConfigScreen;
 
 import java.util.function.BiConsumer;
 
@@ -24,5 +30,21 @@ public class ClientEvents {
 				context::enqueueWork);
 
 		handler.accept(packet, s2cContext);
+	}
+
+	static void setupClient(FMLClientSetupEvent t) {
+		DashMod.getInstance().client.setupNetwork();
+
+		ModContainer thisMod = ModList.get().getModContainerById("dtdash")
+				.orElseThrow(() -> new RuntimeException("Double Tap Dash cannot find... double tap dash?!"));
+
+		thisMod.registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () -> new ConfigGuiHandler.ConfigGuiFactory(
+				(mc, scrn) -> new DashConfigScreen(scrn)
+		));
+
+		DashMod.getInstance().client.onRegisterKeyMappings(k -> {
+			ClientRegistry.registerKeyBinding(k);
+			return k;
+		});
 	}
 }
--- src/main/java/valoeghese/dash/forge/DashMod.java
+++ src/main/java/valoeghese/dash/forge/DashMod.java
@@ -1,19 +1,13 @@
 package valoeghese.dash.forge;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.ClientRegistry;
-import net.minecraftforge.client.ConfigGuiHandler;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModContainer;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import valoeghese.dash.Dash;
 import valoeghese.dash.client.DashClient;
-import valoeghese.dash.client.screen.DashConfigScreen;
 
 @Mod(DashMod.MOD_ID)
 public class DashMod {
@@ -34,27 +28,14 @@ public class DashMod {
 		// set up mod events
 		final IEventBus eventHandler = FMLJavaModLoadingContext.get().getModEventBus();
 		eventHandler.addListener(this::setupCommon);
-		eventHandler.addListener(this::setupClient);
-	}
+
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			eventHandler.addListener(ClientEvents::setupClient);
+		}
+	}	
 
 	final Dash common;
 	DashClient client;
-
-	private void setupClient(FMLClientSetupEvent t) {
-		client.setupNetwork();
-
-		ModContainer thisMod = ModList.get().getModContainerById("dtdash")
-				.orElseThrow(() -> new RuntimeException("Double Tap Dash cannot find... double tap dash?!"));
-
-		thisMod.registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () -> new ConfigGuiHandler.ConfigGuiFactory(
-				(mc, scrn) -> new DashConfigScreen(scrn)
-		));
-
-		client.onRegisterKeyMappings(k -> {
-			ClientRegistry.registerKeyBinding(k);
-			return k;
-		});
-	}
 
 	private void setupCommon(FMLCommonSetupEvent t) {
 		common.setupNetwork();
